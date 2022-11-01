15fe986422ca4e6289ffbef2eed87e077edd2454
reynisson <reynissonkristofer@gmail.com>
2022-11-01 17:21:11 +0100

    Started to fill in the training of our model

---
+++ src/weight_diffusion/ofga/train.py
@@ -0,0 +1,21 @@
+import glob
+import torch
+
+# Set training hyperparameters
+TRAINING_BATCH_SIZE = 5
+EPOCHS = 2
+LEARNING_RATE = 0.0001
+
+# Set manual seed
+torch.manual_seed(1337)
+
+# TODO define training data transformations
+training_data_transformations = []
+
+# TODO load training set and load it into a custom dataset
+training_data = []
+training_data = []
+print("number of training samples", len(training_data))
+
+# TODO examine data
+
