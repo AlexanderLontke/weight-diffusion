1008f987f59c0bc6ed9e31179d25021c2bb071b0
reynisson <reynissonkristofer@gmail.com>
2022-12-08 15:57:34 +0100

    Incremental add to evaluation

--- src/weight_diffusion/ofga/evaluation.py
+++ src/weight_diffusion/ofga/evaluation.py
@@ -8,10 +8,12 @@ from ray.tune.logger import DEFAULT_LOGGERS
 from ray.tune.integration.wandb import WandbLogger
 import ghrp.sampling_auxiliaries.sample_finetune_auxiliaries
 from ldm.util import instantiate_from_config
+import wandb
 
 
-def evaluate_generated_models(epochs: int, prompt: str, models: List[mnist_task.CNN], input, target):
+def evaluate_generated_models(epochs: int, prompt: str, models: mnist_task.CNN, input, target):
     k = len(models)
+    wandb.init()
 
     for model in models:
         _finetune_model(model, epochs)
@@ -19,13 +21,17 @@ def evaluate_generated_models(epochs: int, prompt: str, models: List[mnist_task.
         # TODO record evaluation metrics 
         # TODO save models?
 
-
 def _finetune_model(model, epochs):
     # TODO finetune for requiested epochs, our code or Konstantin's? 
     tune()
 
-def _evaluate_model(model, input, target):
-    # TODO evaluate model accuracy, our code or Konstantin's? 
+def _evaluate_model(model: mnist_task.CNN, input, target):
+    loss, accuracy = mnist_task.test_step(input, target, model)
+    # TODO save evaluation, can't always rely on lighting?
+
+def _calculate_prompt_alignment(prompt: str, results_dict: dict):
+    # TODO calculate prompt alignment
+
 
 # TODO add PromptAlignmentCallback to trainer
 class PromptAlignmentCallback(Callback):
@@ -42,6 +48,7 @@ class PromptAlignmentCallback(Callback):
         encoder_checkpoint = torch.load(encoder_checkpoint_path)
         self.encoder.model.load_state_dict(encoder_checkpoint)
 
+    # TODO which function to overwrite 
     def on_validation_epoch_start(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule") -> None:
         noise = ...  # Generate from shape
         mnist_model_checkpoints_encoded = [pl_module(noise, prompt) for prompt in self.list_of_prompts]
@@ -49,6 +56,8 @@ class PromptAlignmentCallback(Callback):
         results = {}
         for checkpoint in mnist_model_checkpoints:
             current_model = mnist_task.CNN(**self.model_config).load_state_dict(state_dict=checkpoint)
+
             # TODO evaluate current_model on MNIST dataloader
             # TODO store results in dictionary and save them
+            # TODO use lightning module to log
         pass
