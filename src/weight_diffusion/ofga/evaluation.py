17ae5b85f16c05de11c0014ded1674624b7d9b30
reynisson <reynissonkristofer@gmail.com>
2022-12-08 14:20:15 +0100

    TODOs added to eval

--- src/weight_diffusion/ofga/evaluation.py
+++ src/weight_diffusion/ofga/evaluation.py
@@ -6,35 +6,28 @@ import ray
 from ray import tune
 from ray.tune.logger import DEFAULT_LOGGERS
 from ray.tune.integration.wandb import WandbLogger
-from ghrp.sampling_auxiliaries.sample_finetune_auxiliaries import *
+import ghrp.sampling_auxiliaries.sample_finetune_auxiliaries
 from ldm.util import instantiate_from_config
 
-# TODO add PromptAlignmentCallback to trainer
-def evaluate_generated_models(n: int, prompt: str, models: List[mnist_task.CNN]):
+
+def evaluate_generated_models(epochs: int, prompt: str, models: List[mnist_task.CNN], input, target):
     k = len(models)
-    
-    if ray.is_initialized():
-        ray.shutdown()
 
-    finetune(
-        project=f"{source}_to_{target}",
-        population_path=population_path,
-        path_to_samples=path_to_samples,
-        path_target_zoo=source_zoo_path,
-        model_config_path=model_config_path,
-        model_config=take_config_from,
-        no_samples=no_samples,
-        training_epochs=n,
-        cpus=cpus,
-        skip=["uniform", "train", "kde_z_train"],
-    )
-
-    # TODO Fine tune them for n steps
-    # TODO Evaluate their performance during training on MNIST
-    # TODO Return dict of final classification results
-    pass
+    for model in models:
+        _finetune_model(model, epochs)
+        _evaluate_model(model, input, target)
+        # TODO record evaluation metrics 
+        # TODO save models?
 
 
+def _finetune_model(model, epochs):
+    # TODO finetune for requiested epochs, our code or Konstantin's? 
+    tune()
+
+def _evaluate_model(model, input, target):
+    # TODO evaluate model accuracy, our code or Konstantin's? 
+
+# TODO add PromptAlignmentCallback to trainer
 class PromptAlignmentCallback(Callback):
     def __init__(self, list_of_prompts, checkpoints_shape, model_config, dataloader, encoder_config):
         self.list_of_prompts = list_of_prompts
@@ -57,5 +50,5 @@ class PromptAlignmentCallback(Callback):
         for checkpoint in mnist_model_checkpoints:
             current_model = mnist_task.CNN(**self.model_config).load_state_dict(state_dict=checkpoint)
             # TODO evaluate current_model on MNIST dataloader
-            # TODO store results in dictionary
+            # TODO store results in dictionary and save them
         pass
