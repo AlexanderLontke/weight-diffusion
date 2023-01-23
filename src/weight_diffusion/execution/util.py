acb0791551ee2b16a7280c49786f00611d7eb059
AlexanderLontke <77338841+AlexanderLontke@users.noreply.github.com>
2023-01-23 13:57:13 +0100

    Added minor bug fixes

--- src/weight_diffusion/execution/util.py
+++ src/weight_diffusion/execution/util.py
@@ -42,7 +42,7 @@ def load_model_from_config(config, ckpt, verbose=False):
     if "global_step" in pl_sd:
         print(f"Global Step: {pl_sd['global_step']}")
     sd = pl_sd["state_dict"]
-    model = instantiate_from_config(config.model)
+    model = instantiate_from_config(config["model"])
     m, u = model.load_state_dict(sd, strict=False)
     if len(m) > 0 and verbose:
         print("missing keys:")
@@ -51,6 +51,6 @@ def load_model_from_config(config, ckpt, verbose=False):
         print("unexpected keys:")
         print(u)
 
-    model.cuda()
+    model = model.to(torch.device(config["device"]))
     model.eval()
     return model
